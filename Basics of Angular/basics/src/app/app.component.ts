aca7d385b59aa0acd09f3f0a0ce0ea338f486998
waqas <wmunir232@gmail.com>
2024-04-08 11:27:43 +0500

    Made the first component

--- Basics of Angular/basics/src/app/app.component.ts
+++ Basics of Angular/basics/src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyContainerComponent } from './myContainer/myContainer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+// @NgModule({
+//   declarations: [AppComponent, MyContainerComponent],
+// })
 export class AppComponent {
   title = 'World';
 }
---
+++ Basics of Angular/basics/src/app/myContainer/myContainer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myContainer',
+  standalone: true,
+  templateUrl: './myContainer.component.html',
+  styleUrls: ['./myContainer.component.css'],
+})
+export class MyContainerComponent {}

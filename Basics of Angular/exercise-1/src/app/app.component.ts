8cfbc664eb293f21306b1f6e0bf6ad91f4794548
waqas <wmunir232@gmail.com>
2024-04-15 11:44:03 +0500

    completed list components ui

--- Basics of Angular/exercise-1/src/app/app.component.ts
+++ Basics of Angular/exercise-1/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataListComponent } from './component/data-list/data-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DataListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'exercise-1';
+  title1: string = 'User';
+  title2: string = 'Admin';
+
+  user: string[] = ['Ali'];
+  admin: string[] = ['Waqas'];
 }
---
+++ Basics of Angular/exercise-1/src/app/component/data-list/data-list.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-data-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './data-list.component.html',
+  styleUrl: './data-list.component.css',
+})
+export class DataListComponent {
+  @Input() title: string = '';
+  @Input() users: string[] = [];
+}

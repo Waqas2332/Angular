c13d35ca959f4f3d65bbf3799adc3fb340497e44
waqas <wmunir232@gmail.com>
2024-04-16 09:45:11 +0500

    added font awesome for icons

--- angular-crash-course/tasks-list/src/app/components/task-item/task-item.component.ts
+++ angular-crash-course/tasks-list/src/app/components/task-item/task-item.component.ts
@@ -1,14 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../Task';
 import { TASKS } from '../../moct-task';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css',
 })
 export class TaskItemComponent {
   @Input() task: Task = TASKS[0];
+  faTimes = faTimes;
 }

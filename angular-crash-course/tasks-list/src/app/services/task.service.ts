b9427a3ff8dff6c9186525543c7b26f7fb766f9d
waqas <wmunir232@gmail.com>
2024-04-16 10:18:55 +0500

    setup http client

--- angular-crash-course/tasks-list/src/app/services/task.service.ts
+++ angular-crash-course/tasks-list/src/app/services/task.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Task } from '../Task';
 import { TASKS } from '../moct-task';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }

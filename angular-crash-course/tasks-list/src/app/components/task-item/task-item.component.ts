39745e50b095c58bceb4b8eef928790f6272c8c8
waqas <wmunir232@gmail.com>
2024-04-17 09:58:29 +0500

    update tasks

--- angular-crash-course/tasks-list/src/app/components/task-item/task-item.component.ts
+++ angular-crash-course/tasks-list/src/app/components/task-item/task-item.component.ts
@@ -15,10 +15,15 @@ import { CommonModule } from '@angular/common';
 export class TaskItemComponent {
   @Input() task: Task = TASKS[0];
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
 
   faTimes = faTimes;
 
   onDelete(task: Task) {
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 }

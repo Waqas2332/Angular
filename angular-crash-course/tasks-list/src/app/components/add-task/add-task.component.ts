fa522ec57e14c8c6134a485927cf0598b56e249b
waqas <wmunir232@gmail.com>
2024-04-17 10:09:08 +0500

    added two way binding

--- angular-crash-course/tasks-list/src/app/components/add-task/add-task.component.ts
+++ angular-crash-course/tasks-list/src/app/components/add-task/add-task.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.css'
+  styleUrl: './add-task.component.css',
 })
 export class AddTaskComponent {
-
+  text: string = '';
+  day: string = '';
+  reminder: boolean = false;
 }

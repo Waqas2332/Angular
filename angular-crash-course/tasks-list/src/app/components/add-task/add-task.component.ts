36cdf7dbe993954cca647e3420ddbb1473b59ebe
waqas <wmunir232@gmail.com>
2024-04-19 10:39:10 +0500

    handled form toggle

--- angular-crash-course/tasks-list/src/app/components/add-task/add-task.component.ts
+++ angular-crash-course/tasks-list/src/app/components/add-task/add-task.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -14,8 +16,17 @@ export class AddTaskComponent {
   text: string = '';
   day: string = '';
   reminder: boolean = false;
+  showAddTask: boolean = true;
+  subscription: Subscription;
 
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
   onSubmit() {
     if (!this.text) {
       alert('Please Add a Task');
